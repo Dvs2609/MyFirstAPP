9414765f3b1aefbff20fc6611c923cf7688e3a18
Dvs2609 <dvs2609@gmail.com>
2022-11-14 09:41:13 +0100

    Primer Commit: creación de estructura, prueba priemr commit para WebView

--- app/src/main/java/com/example/myapplicatiom/MainActivity2.java
+++ app/src/main/java/com/example/myapplicatiom/MainActivity2.java
@@ -21,42 +21,12 @@ public class MainActivity2 extends AppCompatActivity {
     //private ItemData[] itemsData;
     //private ItemData item;
 
-    List<ItemData> itemsData = Collections.emptyList();
-    RecyclerView recycler;
-
-
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        //RECYCLER VIEW
-        recycler = findViewById(R.id.recyclerID);
-
-        List<ItemData> data = AsignarDatos();
-/*
-        for(int i = 0;i < 4;i++) {
-            itemsData[i] = new  ItemData("dato: "+i+" ", R.id.image_item);
-        }
-*/
-        ImageAdapter Adapter = new ImageAdapter(data, getApplication());
-        recycler.setAdapter(Adapter);
-        //recycler.seton
-        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        //recycler.setLayoutManager(new GridLayoutManager(this,2));
-
     }
 
-    private List<ItemData> AsignarDatos() {
-        List<ItemData> data = new ArrayList<>();
-        data.add(new ItemData("Hola", R.drawable.atosimage));
-        data.add(new ItemData("Esta", R.drawable.atosimage));
-        data.add(new ItemData("Es mi", R.drawable.atosimage));
-        data.add(new ItemData("Aplicacion", R.drawable.atosimage));
-        data.add(new ItemData("RecyclerView", R.drawable.atosimage));
-
-
-        return data;
-    }
 }
