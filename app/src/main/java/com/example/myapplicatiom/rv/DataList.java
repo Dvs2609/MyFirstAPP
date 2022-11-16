4fc7cd03e0240254e8ecf2bdeb7bce5173d752f4
Dvs2609 <dvs2609@gmail.com>
2022-11-16 13:39:24 +0100

    Commit SQLite: Creación de una listapersona en vez de muchos Arraylists

--- app/src/main/java/com/example/myapplicatiom/rv/DataList.java
+++ app/src/main/java/com/example/myapplicatiom/rv/DataList.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.example.myapplicatiom.R;
 import com.example.myapplicatiom.db.DbHelper;
+import com.example.myapplicatiom.db.Persona;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,7 +22,8 @@ import java.util.Comparator;
 
 public class DataList extends AppCompatActivity {
     RecyclerView recyclerView;
-    ArrayList<String> id, dni, name, surname, edad, direc;
+    //ArrayList<String> personas, id, dni, name, surname, edad, direc;
+    ArrayList<Persona> listaPersona;
     DbHelper sqLiteDatabase;
     RecyclerViewAdapter adapter;
 
@@ -32,35 +34,44 @@ public class DataList extends AppCompatActivity {
 
         sqLiteDatabase = new DbHelper(this);
 
-        id = new ArrayList<>();
+        listaPersona = new ArrayList<>();
+
+        /*id = new ArrayList<>();
         dni = new ArrayList<>();
         name = new ArrayList<>();
         surname = new ArrayList<>();
         edad = new ArrayList<>();
-        direc = new ArrayList<>();
+        direc = new ArrayList<>();*/
 
         recyclerView = findViewById(R.id.recyclerview);
-        adapter = new RecyclerViewAdapter(this,id, dni, name, surname, edad, direc);
-        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //adapter = new RecyclerViewAdapter(this,id, dni, name, surname, edad, direc);
         MostrarDatos();
 
-
+        adapter = new RecyclerViewAdapter(this, listaPersona);
+        recyclerView.setAdapter(adapter);
+        
     }
 
     private void MostrarDatos() {
+
+        listaPersona = new ArrayList<>();
         Cursor cursor = sqLiteDatabase.getAllData();
+
         if(cursor.getCount()== 0){
             Toast.makeText(DataList.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
         }else{
             while(cursor.moveToNext()){
-                id.add(cursor.getString(0));
-                dni.add(cursor.getString(1));
-                name.add(cursor.getString(2));
-                surname.add(cursor.getString(3));
-                edad.add(cursor.getString(4));
-                direc.add(cursor.getString(5));
+                Persona p = new Persona();
+                p.setId(cursor.getString(0));
+                p.setDni(cursor.getString(1));
+                p.setNombre(cursor.getString(2));
+                p.setApellido(cursor.getString(3));
+                p.setEdad(cursor.getString(4));
+                p.setDireccion(cursor.getString(5));
+                listaPersona.add(p);
             }
         }
+
     }
 }
--- app/src/main/java/com/example/myapplicatiom/rv/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/myapplicatiom/rv/RecyclerViewAdapter.java
@@ -10,14 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplicatiom.R;
+import com.example.myapplicatiom.db.Persona;
 
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
     private Context context;
-    private ArrayList id_id, dni_id, name_id, surname_id, edad_id, direc_id;
+    //private ArrayList id_id, dni_id, name_id, surname_id, edad_id, direc_id;
+    ArrayList<Persona> listaPersona;
 
-    public RecyclerViewAdapter(Context context,ArrayList id_id, ArrayList dni_id, ArrayList name_id, ArrayList surname_id, ArrayList edad_id, ArrayList direc_id) {
+    public RecyclerViewAdapter(Context context, ArrayList<Persona> listaPersona) {
+        this.context = context;
+        this.listaPersona = listaPersona;
+    }
+
+
+/*public RecyclerViewAdapter(Context context,ArrayList id_id, ArrayList dni_id, ArrayList name_id, ArrayList surname_id, ArrayList edad_id, ArrayList direc_id) {
         this.context = context;
         this.id_id = id_id;
         this.dni_id = dni_id;
@@ -25,7 +33,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         this.surname_id = surname_id;
         this.edad_id = edad_id;
         this.direc_id = direc_id;
-    }
+    }*/
 
     @NonNull
     @Override
@@ -36,17 +44,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.id_id.setText(String.valueOf(id_id.get(position)));
-        holder.dni_id.setText(String.valueOf(dni_id.get(position)));
-        holder.name_id.setText(String.valueOf(name_id.get(position)));
-        holder.surname_id.setText(String.valueOf(surname_id.get(position)));
-        holder.edad_id.setText(String.valueOf(edad_id.get(position)));
-        holder.direc_id.setText(String.valueOf(direc_id.get(position)));
+        holder.id_id.setText(String.valueOf(listaPersona.get(position).getId()));
+        holder.dni_id.setText(String.valueOf(listaPersona.get(position).getDni()));
+        holder.name_id.setText(String.valueOf(listaPersona.get(position).getNombre()));
+        holder.surname_id.setText(String.valueOf(listaPersona.get(position).getApellido()));
+        holder.edad_id.setText(String.valueOf(listaPersona.get(position).getEdad()));
+        holder.direc_id.setText(String.valueOf(listaPersona.get(position).getDireccion()));
     }
 
     @Override
     public int getItemCount() {
-        return id_id.size();
+        return listaPersona.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
