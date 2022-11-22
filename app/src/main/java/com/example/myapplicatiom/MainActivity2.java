52edbda10e05260fddf210cc59191c7c60f4423e
Dvs2609 <dvs2609@gmail.com>
2022-11-22 10:55:45 +0100

    Commit Retrofit: mostrar datos de una API. terminado

--- app/src/main/java/com/example/myapplicatiom/MainActivity2.java
+++ app/src/main/java/com/example/myapplicatiom/MainActivity2.java
@@ -1,105 +1,57 @@
 package com.example.myapplicatiom;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-
-import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import android.widget.Toast;
 
-import com.example.myapplicatiom.rf.APIservice;
-import com.example.myapplicatiom.rf.APIutils;
-import com.example.myapplicatiom.rf.RetrofitClass;
-import com.example.myapplicatiom.rv.RecyclerViewAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity2 extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    LinearLayoutManager layoutManager;
-    RecyclerViewAdapter adapter;
-    TextView name;
-
-    private APIservice mService;
-    List<Character> listaPersona = new ArrayList<>();
-    private static final String BASE_URL = "https://breakingbadapi.com/api/characters?limit=5";
-    @SuppressLint("MissingInflatedId")
+    EditText txtnombre, txtapellido;
+    Button buttonShared;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         getSupportActionBar().hide();
 
-        recyclerView = findViewById(R.id.RecyclerView);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        txtnombre = findViewById(R.id.Nombre_shared);
+        txtapellido = findViewById(R.id.Apellido_Shared);
+        buttonShared = findViewById(R.id.button_Shared);
 
+        SharedPreferences datos = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        txtnombre.setText(datos.getString("nombre", ""));
+        txtapellido.setText(datos.getString("apellido", ""));
 
-        APIservice apiservice = RetrofitClass.getClient().create(APIservice.class);
-
-        Call<List<Character>> call = apiservice.getCharacterLimit5();
-        call.enqueue(new Callback<List<Character>>() {
+        buttonShared.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<List<Character>> call, Response<List<Character>> response) {
-                //List<Character> characters = response.body().getListcharacter();
-                if (response.isSuccessful() && response.body() != null && !response.body().isEmpty()) {
-
-                    listaPersona = response.body();
-                    //listaPersona.addAll(response.body());
-                }
-                adapter = new RecyclerViewAdapter(getApplicationContext(), listaPersona);
-                recyclerView.setAdapter(adapter);
-
-            }
-            @Override
-            public void onFailure(Call<List<Character>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                guardardatos(view);
             }
         });
-
-
-
-        //loadAnswers();
 
     }
 
+    public void guardardatos(View view){
+        SharedPreferences datos = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = datos.edit();
 
+        editor.putString("nombre", txtnombre.getText().toString());
+        editor.putString("apellido", txtapellido.getText().toString());
 
-    /*private void loadAnswers() {
-        APIservice.getAnswers().enqueue(new Callback<List<Character>>() {
-            @Override
-            public void onResponse(Call<List<Character>> call, Response<List<Character>> response) {
-
-                List<Character> posts = response.body();
-                name = findViewById(R.id.TVidShow);
-
-                for (Character post : posts){
-                    String nombre = post.getName();
-                    name.append(nombre);
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Character>> call, Throwable t) {
-
-            }
-
-
-        });
-    }*/
+        //editor.apply();
+        editor.commit();
+        Toast.makeText(this, "Guardado con exito", Toast.LENGTH_SHORT).show();
+    }
 
 }
--- app/src/main/java/com/example/myapplicatiom/rf/APIutils.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplicatiom.rf;
-
-public class APIutils {
-
-    public static APIservice getSOService() {
-        return RetrofitClass.getClient().create(APIservice.class);
-    }
-}
